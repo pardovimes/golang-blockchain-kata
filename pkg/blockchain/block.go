621b6fea07d080a268e3640321a841f00d9cfbad
carlos pardo <carlos.pardo@atrapalo.com>
2019-03-15 13:30:32 +0100

    block model

---
+++ pkg/blockchain/block.go
@@ -0,0 +1,29 @@
+package blockchain
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"strconv"
+	"time"
+)
+
+// Block ...
+type Block struct {
+	Index        int
+	timestamp    time.Time
+	data         string
+	hash         string
+	previousHash string
+	nonce        uint64
+}
+
+func (b Block) CalculateHash() string {
+	hasher := sha256.New()
+	stringToHash := strconv.Itoa(b.Index) +
+		b.previousHash +
+		b.timestamp.String() +
+		strconv.Itoa(int(b.nonce))
+	hasher.Write([]byte(stringToHash))
+	sha := hex.EncodeToString(hasher.Sum(nil))
+	return sha
+}
